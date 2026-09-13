ff533793a2d2b6538f10756ddd400a6d3bbb90bc
Zhagui-Jason <jason.zhaguil.est@uets.edu.ec>
2026-09-13 18:35:27 -0500

    feat(03_unions_narrowing): Implementación de typeof, analisis de estados y usando switch

--- src/03_unions_narrowing.ts
+++ src/03_unions_narrowing.ts
@@ -18,19 +18,13 @@
  * 3. Ejecuta en tu terminal: `pnpm run start:03` para verificar los tests.
  */
 
-// ============================================================================
-// PASO 1: Type Narrowing Básico con typeof
-// ============================================================================
-/**
- * TODO: Implementa la función `formatearIdentificador`.
- * - Recibe un `id` que puede ser `string` o `number`.
- * - Si es `string`, retornar: `ID-ALFANUMERICO-` seguido del texto en MAYÚSCULAS.
- * - Si es `number`, retornar: `ID-NUMERICO-#` seguido del número relleno con ceros a 6 dígitos (ej: 45 -> "000045").
- *   (Pista: usa id.toFixed(0).padStart(6, "0"))
- */
+
 export function formatearIdentificador(id: string | number): string {
-  // 👇 TODO: Escribe tu lógica con if (typeof id === "string") y reemplaza el return "":
-  return "";
+  if(typeof id === "string"){
+    return `ID-ALFANUMERICO-${id.toUpperCase()}`;
+  }
+
+  return `ID-NUMERICO-#${id.toFixed(0).padStart(6, "0")}`;
 }
 
 // ============================================================================
@@ -59,14 +53,13 @@ export type EstadoPantalla<T> =
   | EstadoExito<T>
   | EstadoError;
 
-/**
- * TODO: Implementa `renderizarEstadoUI`.
- * Utiliza un `switch (estado.status)`:
- * - Si status === "LOADING": Retornar `⏳ Cargando datos (${estado.porcentaje}%)...`
- * - Si status === "SUCCESS": Retornar `🎉 Datos cargados con éxito a las ${estado.hora}`
- * - Si status === "ERROR": Retornar `❌ Error ${estado.codigo}: ${estado.mensaje}`
- */
 export function renderizarEstadoUI<T>(estado: EstadoPantalla<T>): string {
-  // 👇 TODO: Escribe tu switch(estado.status) aquí y reemplaza el return "":
-  return "";
+  switch (estado.status) {
+    case "LOADING":
+      return `⏳ Cargando datos (${estado.porcentaje}%)...`;
+    case "SUCCESS":
+      return `🎉 Datos cargados con éxito a las ${estado.hora}`;
+    case"ERROR":
+      return `❌ Error ${estado.codigo}: ${estado.mensaje}`;
+  }
 }
